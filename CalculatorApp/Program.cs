b18136d66d482a05c46d3733acead4b379d9f6e7
UlyanaMalt <ulyanka_maltseva@bk.ru>
2026-05-20 20:22:46 +0300

    Добавьте файлы проекта.

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Add(2, 3));
+        }
+    }
+}
